acf7a9e9f5533613ae778fc618fcb5a7d97e3db4
Neeraj Baid <wdaareg@gmail.com>
2015-04-14 09:43:54 -0700

    Create project.

---
+++ WWDC2015/WWDC2015/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWDC2015
+//
+//  Created by Neeraj Baid on 4/14/15.
+//  Copyright (c) 2015 Neeeraj Baid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWDC2015/WWDC2015/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WWDC2015
+//
+//  Created by Neeraj Baid on 4/14/15.
+//  Copyright (c) 2015 Neeeraj Baid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

b0e0d68912400625baf9cf73a0d477a6b7e115bf
Neeraj Baid <wdaareg@gmail.com>
2015-04-14 22:06:34 -0700

    Misc changes.

--- WWDC2015/WWDC2015/main.m
+++ WWDC2015/WWDC2015/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  WWDC2015
-//
-//  Created by Neeraj Baid on 4/14/15.
-//  Copyright (c) 2015 Neeeraj Baid. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "NBAppDelegate.h"
 
